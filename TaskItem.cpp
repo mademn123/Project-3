0232e22743c38aeff77e6b6fc86f21f8fbe932a2
aarithi123 <148919216+aarithi123@users.noreply.github.com>
2024-11-30 12:48:33 -0500

    Task Item Class initial commit

---
+++ TaskItem.cpp
@@ -0,0 +1,55 @@
+//
+// Created by aarit on 11/30/2024.
+//
+
+#include "TaskItem.h"
+#include "TaskItem.h"
+#include <QPainter>
+#include <cmath>
+
+TaskItem::TaskItem(Task* task, QGraphicsItem* parent)
+        : QGraphicsEllipseItem(0, 0, 80, 80, parent), task(task) {
+    setFlag(QGraphicsItem::ItemIsMovable);
+    setFlag(QGraphicsItem::ItemSendsGeometryChanges);
+}
+
+QVariant TaskItem::itemChange(GraphicsItemChange change, const QVariant& value) {
+    if (change == ItemPositionHasChanged && scene()) {
+        updateEdges();
+    }
+    return QGraphicsEllipseItem::itemChange(change, value);
+}
+
+void TaskItem::addEdge(TaskItem* endItem) {
+    edges.append(endItem);
+}
+
+void TaskItem::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) {
+    QGraphicsEllipseItem::paint(painter, option, widget);
+
+    for (TaskItem* endItem : edges) {
+        QPointF startPoint = mapToScene(rect().center());
+        QPointF endPoint = endItem->mapToScene(endItem->rect().center());
+
+        painter->setPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+        painter->drawLine(mapFromScene(startPoint), mapFromScene(endPoint));
+
+        double angle = std::atan2(endPoint.y() - startPoint.y(), endPoint.x() - startPoint.x());
+        QPointF arrowP1 = endPoint - QPointF(15 * std::cos(angle - M_PI / 6),
+                                             15 * std::sin(angle - M_PI / 6));
+        QPointF arrowP2 = endPoint - QPointF(15 * std::cos(angle + M_PI / 6),
+                                             15 * std::sin(angle + M_PI / 6));
+
+        QPolygonF arrowHead;
+        arrowHead << mapFromScene(endPoint) << mapFromScene(arrowP1) << mapFromScene(arrowP2);
+        painter->setBrush(Qt::black);
+        painter->drawPolygon(arrowHead);
+    }
+}
+
+void TaskItem::updateEdges() {
+    update();
+    for (TaskItem* endItem : edges) {
+        endItem->update();
+    }
+}
---
+++ TaskItem.h
@@ -0,0 +1,27 @@
+//
+// Created by aarit on 11/30/2024.
+//
+
+#ifndef TASKSCHEDULERTEMP_TASKITEM_H
+#define TASKSCHEDULERTEMP_TASKITEM_H
+
+#include <QObject>
+#include <QGraphicsEllipseItem>
+#include "Task.h"
+
+class TaskItem : public QObject, public QGraphicsEllipseItem {
+    Q_OBJECT
+
+public:
+    TaskItem(Task* task, QGraphicsItem* parent = nullptr);
+
+    QVariant itemChange(GraphicsItemChange change, const QVariant& value) override;
+    void addEdge(TaskItem* endItem);
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+    void updateEdges();
+
+    Task* task;
+    QList<TaskItem*> edges;
+};
+
+#endif // TASKITEM_H
